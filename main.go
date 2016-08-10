016dfe949e0244fd725ea75294f352265d1db719
EvilKhaosKat <evilkhaoskat@gmail.com>
2016-08-10 23:15:03 +0400

    Basic iteration logic

--- main.go
+++ main.go
@@ -3,14 +3,43 @@ package main
 import (
 	_ "github.com/tealeg/xlsx"
 	"io/ioutil"
-	"fmt"
+	"strings"
+	"os"
+	"log"
 )
 
 const Dir = "files"
 
+type result struct {
+	year    int
+	month   int
+
+	value   int
+
+	updated bool
+	delta   int
+}
+
 func main() {
-	files, _ := ioutil.ReadDir(Dir)
-	for _, f := range files {
-		fmt.Println(f.Name())
+	resultsChan := make(chan result)
+
+	files, err := ioutil.ReadDir(Dir)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	for _, f := range files {
+		if isSuitableFile(f) {
+			go handleFile(f.Name(), resultsChan)
+		}
+	}
+
+	//var results []result
+}
+func isSuitableFile(file os.FileInfo) bool {
+	return !file.IsDir() && strings.HasSuffix(file.Name(), ".xls")
+}
+
+func handleFile(filename string, resultsChan chan result) {
+	log.Printf("handle file %s", filename)
 }
